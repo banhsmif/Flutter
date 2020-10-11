32e3cedea14cd331f66cf3aa4c023ee547bc7235
banhsmif <2jiro.jp@gmail.com>
2020-10-11 22:58:39 +0700

    micard done

--- mi_card/lib/main.dart
+++ mi_card/lib/main.dart
@@ -3,32 +3,83 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MyApp());
 }
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
+        backgroundColor: Colors.grey[300],
         body: SafeArea(
-          child: Column(
-            children: [
-              CircleAvatar(
-                radius: 50.0,
-                backgroundImage: AssetImage('images/profile.jpg'),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CircleAvatar(
+              radius: 50.0,
+              backgroundImage: AssetImage('images/main.jpg'),
+            ),
+            Text(
+              'Lauren',
+              style: TextStyle(
+                fontSize: 40.0,
+                color: Colors.grey.shade700,
+                fontWeight: FontWeight.bold,
+                fontFamily: 'Pacifico',
               ),
-              Text(
-                  'Juliet',
-                style: TextStyle(
-                  fontSize: 40.0,
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-
+            ),
+            Text(
+                'FLUTTER DEVELOPER',
+              style: TextStyle(
+                fontSize: 15.0,
+                color: Colors.grey.shade500,
+                fontFamily: 'Source Sans Pro',
+                letterSpacing: 2.5,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 10.0,
+              width: 150.0,
+              child: Divider(
+                color: Colors.grey[900],
+              ),
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              child: ListTile(
+                leading: Icon(Icons.call,
+                color: Colors.grey.shade700,),
+                title: Text(
+                  '+84 xxx xxx xx',
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    fontFamily: 'Source Sans Pro',
+                    color: Colors.grey.shade700,
+                  ),
                 ),
               ),
-            ],
-          )
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              child: ListTile(
+              leading: Icon(Icons.mail,
+              color: Colors.grey.shade700,),
+                title: Text(
+                  'mail@mail.com',
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    fontFamily: 'Source Sans Pro',
+                    color: Colors.grey.shade700,
+                    letterSpacing: 1.2,
+                  ),
+                ),
+            ),
+            ),
+          ],
+        ),
         ),
       ),
     );
   }
 }
+
